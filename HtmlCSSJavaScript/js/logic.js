3837ec03814bd842eb3e166669b22fa47d57b8f0
Willian Classen <Wgclassen@gmail.com>
2025-08-21 20:23:22 -0300

    clearButton

--- HtmlCSSJavaScript/js/logic.js
+++ HtmlCSSJavaScript/js/logic.js
@@ -1,5 +1,25 @@
 const screenNumber = document.querySelector(".screen");
 const numberButton = document.querySelectorAll(".number");
+const cButton = document.querySelector(".c");
+const plusButton = document.querySelector(".plus")
+const minusButton = document.querySelector(".minus");
+const timesButton = document.querySelector(".times");
+const divideButton = document.querySelector(".divide");
+const decimalButton = document.querySelector(".decimal");
+const equalsButton = document.querySelector(".equals");
+const deleteButton = document.querySelector(".delete");
+
+numberButton.forEach(button => {
+    button.addEventListener("click", () => addNumber(button));
+});
+cButton.addEventListener("click", clearScreen);
+plusButton.addEventListener("click", addOperation);
+
+
+function addOperation() {
+    let firstNumber = screenNumber.textContent;
+    clearScreen();
+}
 
 function addNumber(button) {
     if (screenNumber.textContent === "0") {
@@ -9,6 +29,7 @@ function addNumber(button) {
     }
 }
 
-numberButton.forEach(button => {
-    button.addEventListener("click", () => addNumber(button));
-});
+function clearScreen() {
+    screenNumber.textContent = "0";
+    adjustFontSize();
+}
