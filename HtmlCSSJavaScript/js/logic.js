7db5e01126d6d2340ab755dfded9dd64661ddd80
Willian Classen <Wgclassen@gmail.com>
2025-08-22 17:44:13 -0300

    OperationsLogic

--- HtmlCSSJavaScript/js/logic.js
+++ HtmlCSSJavaScript/js/logic.js
@@ -1,3 +1,4 @@
+// - DOM - //
 const screenNumber = document.querySelector(".screen");
 const numberButton = document.querySelectorAll(".number");
 const cButton = document.querySelector(".c");
@@ -9,15 +10,35 @@ const decimalButton = document.querySelector(".decimal");
 const equalsButton = document.querySelector(".equals");
 const deleteButton = document.querySelector(".delete");
 
+const Operators = {
+    PLUS: "plus",
+    MINUS: "minus",
+    DIVIDE: "divide",
+    TIMES: "times"
+};
+
+let lastOperator;
+let storedNumber;
+
+
+// - Event Listeners - //
 numberButton.forEach(button => {
     button.addEventListener("click", () => addNumber(button));
 });
 cButton.addEventListener("click", clearScreen);
-plusButton.addEventListener("click", addOperation);
+// plusButton.addEventListener("click", addOperation);
+plusButton.addEventListener("click", () => setOperation(Operators.PLUS));
+minusButton.addEventListener("click", () => setOperation(Operators.MINUS));
+timesButton.addEventListener("click", () => setOperation(Operators.TIMES));
+divideButton.addEventListener("click", () => setOperation(Operators.DIVIDE));
+equalsButton.addEventListener("click", performOperation);
+deleteButton.addEventListener("click", erase);
 
 
-function addOperation() {
-    let firstNumber = screenNumber.textContent;
+// - Functions - //
+function setOperation(Operator) {
+    storedNumber = screenNumber.textContent;
+    lastOperator = Operator;
     clearScreen();
 }
 
@@ -29,7 +50,29 @@ function addNumber(button) {
     }
 }
 
+function performOperation() {
+    if (lastOperator == Operators.PLUS) {
+        screenNumber.textContent = parseFloat(storedNumber) + parseFloat(screenNumber.textContent);
+    }
+    if (lastOperator == Operators.MINUS) {
+        screenNumber.textContent = parseFloat(storedNumber) - parseFloat(screenNumber.textContent);
+    }
+    if (lastOperator == Operators.TIMES) {
+        screenNumber.textContent = parseFloat(storedNumber) * parseFloat(screenNumber.textContent);
+    }
+    if (lastOperator == Operators.DIVIDE) {
+        screenNumber.textContent = parseFloat(storedNumber) / parseFloat(screenNumber.textContent);
+    }
+}
+
+function erase() {
+    if (screenNumber.textContent.length > 1) {
+        screenNumber.textContent = screenNumber.textContent.slice(0, -1);
+    } else {
+        clearScreen();
+    }
+}
+
 function clearScreen() {
     screenNumber.textContent = "0";
-    adjustFontSize();
 }
